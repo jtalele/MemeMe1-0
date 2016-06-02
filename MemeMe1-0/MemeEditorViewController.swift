c92aa39b4fcf60fd5fbdd05f9d147ecc9ebef1a5
Jitendr Talele <Saisha@jitendrs-macbook-pro.local>
2016-06-02 15:18:27 -0400

    added keyboard hight logic

--- MemeMe1-0/MemeEditorViewController.swift
+++ MemeMe1-0/MemeEditorViewController.swift
@@ -13,6 +13,60 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let textFieldArray = [topText, bottomText]
+        configureTextFields(textFieldArray)
+        
+        //Disable the Share button:
+        shareButton.enabled = false
+        cancelButton.enabled = false
+    }
+    
+    func configureTextFields(textFields: [UITextField!]){
+        let memeTextAttributes = [
+            NSStrokeColorAttributeName: UIColor.blackColor(),
+            NSForegroundColorAttributeName: UIColor.whiteColor(),
+            NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+            NSStrokeWidthAttributeName : 4.0
+        ]
+        
+        for textField in textFields{
+            //Configure and position the top textfield
+            textField.textAlignment = .Center
+            textField.delegate = self
+            textField.defaultTextAttributes = memeTextAttributes
+        }
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(true)
+        
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(.Camera)
+        
+        //Subscribe to keyboard notifications:
+        subscribeToKeyboardNotification()
+    }
+    
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyboardSize.CGRectValue().height
+    }
+    
+    func displayKeyboard(notification: NSNotification) {
+        if selectedTextField == bottomText && self.view.frame.origin.y == 0.0 {
+            self.view.frame.origin.y -= getKeyboardHeight(notification)
+        }
+    }
+    
+    func hideKeyboard(notification: NSNotification) {
+        if -self.view.frame.origin.y > 0 {
+            self.view.frame.origin.y = 0
+        }
+    }
+    
+    func subscribeToKeyboardNotification(){
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorViewController.displayKeyboard(_:)), name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(MemeEditorViewController.hideKeyboard(_:)), name: UIKeyboardWillHideNotification, object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +77,9 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var shareButton: UIBarButtonItem!
     @IBOutlet weak var cancelButton: UIBarButtonItem!
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
+    @IBOutlet weak var topText: UITextField!
+    @IBOutlet weak var bottomText: UITextField!
     
     var imagePickerController:UIImagePickerController!
     var memedImage:UIImage!
@@ -43,6 +100,14 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
             imagePickerController.allowsEditing=false
             self.presentViewController(imagePickerController,animated: true, completion: nil)
         }
+    }
+    
+    @IBAction func pickImageFromCamera(sender: AnyObject) {
+        imagePickerController=UIImagePickerController()
+        imagePickerController.delegate=self
+        imagePickerController.sourceType=UIImagePickerControllerSourceType.Camera
+        self.presentViewController(imagePickerController,animated: true, completion: nil)
+        
     }
 
 }
